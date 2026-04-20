ee41f80eb496eda00e201a6cbdb99fe1c47c9545
unit-integrations <dev-integrations@unit.co>
2026-04-20 11:33:18 +0000

    Release 0.0.0

---
+++ UnitSDK.xcframework/ios-arm64/UnitSDK.framework/Headers/UnitSDK.h
@@ -0,0 +1,18 @@
+//
+//  UnitSDK.h
+//  UnitSDK
+//
+//  Created by Alon Shprung on 19/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitSDK.
+FOUNDATION_EXPORT double UnitSDKVersionNumber;
+
+//! Project version string for UnitSDK.
+FOUNDATION_EXPORT const unsigned char UnitSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitSDK/PublicHeader.h>
+
+
