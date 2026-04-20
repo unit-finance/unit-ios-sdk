7ed2aac9c80cadf872099506cab9b22aa3172d43
unit-integrations <dev-integrations@unit.co>
2026-04-20 05:30:11 +0000

    Release 0.0.0

---
+++ UnitCommon.xcframework/ios-arm64/UnitCommon.framework/Headers/UnitCommon.h
@@ -0,0 +1,18 @@
+//
+//  UnitCommon.h
+//  UnitCommon
+//
+//  Created by Alon Shprung on 19/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitCore.
+FOUNDATION_EXPORT double UnitCommonVersionNumber;
+
+//! Project version string for UnitCore.
+FOUNDATION_EXPORT const unsigned char UnitCommonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitCore/PublicHeader.h>
+
+
---
+++ UnitFraud.xcframework/ios-arm64/UnitFraud.framework/Headers/UnitFraud.h
@@ -0,0 +1,18 @@
+//
+//  UnitFraud.h
+//  UnitFraud
+//
+//  Created by Alon Shprung on 19/11/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UnitFraud.
+FOUNDATION_EXPORT double UnitFraudVersionNumber;
+
+//! Project version string for UnitFraud.
+FOUNDATION_EXPORT const unsigned char UnitFraudVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UnitFraud/PublicHeader.h>
+
+
